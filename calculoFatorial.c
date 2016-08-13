934de8dc4d53fa6767bad9b803b43a641292b6f8
Ezequiel losekann drews <Ezequiel losekann drews>
2016-08-12 22:11:03 -0300

    Subindo os códigos com o código completo

---
+++ calculoFatorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+{
+	int valorN, i, resultado;
+	
+	printf("Insira um valor para calcular o fatorial deste numero: ");
+	scanf("%d", &valorN);
+			
+	for(i=0;i<valorN;i++)
+	{		
+		resultado = resultado * (valorN - i);
+	}
+	
+	printf("\n\n resultado eh = %d", resultado);
+}
