edd60d5a005c9da17c94d032ecb1255b1e403f9d
ezequieldrews <ezequiel.losekann@hotmail.com>
2016-08-26 22:13:23 -0300

    subindo modidicações do código de matrizes

--- matrizes.c
+++ matrizes.c
@@ -2,7 +2,7 @@
 
 main()
 {
-	int matriz[4][4], j = 0, k = 0;
+	int matriz[4][4], j = 0, k = 0, i = 0;
 	
 	srand( (unsigned)time(NULL) );
 	
@@ -15,5 +15,52 @@ main()
 			printf("%02d ", matriz[j][k]);
 		}
 		printf("\n");
+	}
+	
+	printf("\n\n\n\n");
+	
+	printf("Diagonal Principal:\n\n");
+	for(j = 0; j < 4; j++)
+	{
+		for(k = 0; k < 4; k++)
+		{
+			//matriz[j][k] = rand()%50;
+			
+		if(j == k)
+			{
+				printf("%3d ", matriz[j][k]);	
+					
+			}		
+			
+			for(i=0;i<4;i++)
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+		
+	}
+		
+	printf("\n\n\n\n\n");
+	
+	printf("Exclusão da Diagonal Principal\n");	
+	for(j = 0; j < 4; j++)
+	{
+		for(k = 0; k < 4; k++)
+		{
+			//matriz[j][k] = rand()%50;
+			
+		if(j == k)
+		{
+			printf("--");					
+		}
+		else
+		{
+			printf("%02d ", matriz[k][j]);
+		}		
+			
+		}
+		printf("\n");
+		
 	}
 }
